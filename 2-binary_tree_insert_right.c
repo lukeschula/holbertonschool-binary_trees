1b5db01a36cd31160633646433742dd870b929f2
root <schulaluke@gmail.com>
2023-12-20 15:45:16 -0800

    bug fix

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -6,13 +6,15 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *add_node;
 
-	if (parent == NULL || value == '\0')
+	if (parent == NULL)
 		return (NULL);
 
 	add_node = malloc(sizeof(binary_tree_t));
 
 	if (add_node == NULL)
+	{	
 		return (NULL);
+	}
 
 	add_node->n = value;
 	add_node->parent = parent;
@@ -20,11 +22,10 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	add_node->right = parent->right;
 
 	if (parent->right != NULL)
-	{
 		parent->right->parent = add_node;
-		add_node->right = add_node;
-	}
-	parent->left = add_node;
+
+	add_node->right = add_node;
+	
 
 	return (add_node);
 }
