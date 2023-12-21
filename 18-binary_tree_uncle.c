4c8fed4c799e793d90b6bba7e9519a8d95b9715b
root <schulaluke@gmail.com>
2023-12-21 15:53:07 -0800

    betty fix

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,6 +1,8 @@
 #include "binary_trees.h"
 /**
- *
+ * binary_tree_uncle - finds the uncle of a node
+ * @node: pointer to node in question
+ * Return: pointer to uncle
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
