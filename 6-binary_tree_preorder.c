0b4af6d2ad03d5c84db819b4633e0893927b17a9
root <schulaluke@gmail.com>
2023-12-21 14:59:52 -0800

    betty fix

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,6 +1,9 @@
 #include "binary_trees.h"
 /**
- *
+ * binary_tree_preorder - function
+ * description - pre-order traversal
+ * @tree: pointer to the root
+ * @func: is a pointer to function
  *
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
