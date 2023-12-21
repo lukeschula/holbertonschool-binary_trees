ad27d6327ea4c73ea8a3d5854288d2e750f33af3
root <schulaluke@gmail.com>
2023-12-21 15:24:30 -0800

    betty fix

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,6 +1,8 @@
 #include "binary_trees.h"
 /**
- *
+ * binary_tree_nodes - counts the nodes with at least 1 child
+ * @tree: pointer to the root node of the tree
+ * Return: Number of nodes with children
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
@@ -12,7 +14,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (tree->left == NULL && tree->right == NULL)
 		return (0);
 
-	left_nodes= binary_tree_nodes(tree->left);
+	left_nodes = binary_tree_nodes(tree->left);
 	right_nodes = binary_tree_nodes(tree->right);
 
 	return (left_nodes + right_nodes + 1);
