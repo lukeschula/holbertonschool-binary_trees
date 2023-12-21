f03bbe08fba7d0ec25cb23a9a503a9b9a3cf98db
root <schulaluke@gmail.com>
2023-12-20 16:24:50 -0800

    added logic

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+/**
+ *
+ *
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+	{
+		return (0);
+	}
+
+	if (node != NULL && !node->left && !node->right)
+	{
+		return (1);
+
+	}
+	return (0);
+}
