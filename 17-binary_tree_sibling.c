ddadb8ee40fbdb7a4eae9047e3787ffe4d3d0a6e
root <schulaluke@gmail.com>
2023-12-20 20:54:48 -0800

    completed

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+/**
+ *
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || !node->parent)
+	{
+		return (NULL);
+	}
+
+	if (node->parent->left == node)
+	{
+		return (node->parent->right);
+	}
+	else
+	{	
+		return (node->parent->left);
+	}
+	return (NULL);
+}
