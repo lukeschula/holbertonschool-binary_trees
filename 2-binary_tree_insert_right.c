f5b11903008bb690b93b57d7badee79b5dde64e6
root <schulaluke@gmail.com>
2023-12-21 14:44:45 -0800

    betty fix

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -10,13 +10,13 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	binary_tree_t *add_node;
 
 	if (parent == NULL)
-	{	
+	{
 		return (NULL);
 	}
 	add_node = malloc(sizeof(binary_tree_t));
 
 	if (add_node == NULL)
-	{	
+	{
 		return (NULL);
 	}
 
@@ -34,7 +34,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		parent->right->parent = add_node;
 		parent->right = add_node;
 	}
-	
+
 
 	return (add_node);
 }
