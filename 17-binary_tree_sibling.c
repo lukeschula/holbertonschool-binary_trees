e25540013dae0e5ebfcd8554b07199fd877915c1
root <schulaluke@gmail.com>
2023-12-21 15:50:01 -0800

    betty fix

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,6 +1,11 @@
 #include "binary_trees.h"
 /**
+ *  binary_tree_sibling - function
+ *  description - function that finds the sibling of a node
  *
+ *  @node: Pointer to the node to find the sibling
+ *
+ *  Return: Sibling node
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
