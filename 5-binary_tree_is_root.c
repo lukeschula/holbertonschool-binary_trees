cbc1cb7cd4ac392ef21718026c62d722593e091b
root <schulaluke@gmail.com>
2023-12-20 16:28:13 -0800

    bug fix

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -6,14 +6,14 @@
 int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node == NULL)
-	{
+	
 		return (0);
-	}
+	
 
-	if (node != NULL && !node->left && !node->right)
-	{
+	if (node-> parent == NULL)
+	
 		return (1);
-
-	}
-	return (0);
+	else
+		
+		return (0);
 }
