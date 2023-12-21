67f2d92a9dc7a50ab6fa164feb2089bfa60af1dc
root <schulaluke@gmail.com>
2023-12-21 15:39:40 -0800

    betty fix

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,8 @@
 #include "binary_trees.h"
 /**
- *
+ * binary_tree_leaf - check the leaf
+ * @node: passed parent
+ * Return: 1 if is leafe 0 otherwise
  *
  */
 int binary_tree_leaf(const binary_tree_t *new_node)
@@ -18,6 +20,11 @@ int binary_tree_leaf(const binary_tree_t *new_node)
 	return (0);
 }
 /**
+ * binary_tree_depth - measures the height of a binary tree
+ *
+ * @tree: pointer to the root node of the tree to measure the height
+ * Return: the height
+ *
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
@@ -37,6 +44,10 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	return (1 + l);
 }
 /**
+ * binary_tree_balance -  the balance factor
+ * @tree: pointer to nod
+ * Return: the measure balancer factor of df
+ *
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
